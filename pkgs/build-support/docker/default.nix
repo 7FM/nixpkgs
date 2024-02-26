de254f12d81dbda26b188723fc765ceb81a67e03
7FM <41307817+7FM@users.noreply.github.com>
2024-02-26 11:26:31 +0100

    dockerTools: force interactive mode to actually load the rcfile

--- pkgs/build-support/docker/default.nix
+++ pkgs/build-support/docker/default.nix
@@ -1235,7 +1235,7 @@ rec {
           else [ shell rcfile ];
         config.WorkingDir = sandboxBuildDir;
         config.Env = lib.mapAttrsToList (name: value: "${name}=${value}") envVars;
-        config.Entrypoint = config.Cmd ++ [ "-c" ];
+        config.Entrypoint = config.Cmd ++ [ "-ci" ];
       };
 
   # Wrapper around streamNixShellImage to build an image from the result
